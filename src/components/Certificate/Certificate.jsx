f38468c18a9c30f0fb235b1ca6ff3450103f97c3
Tusshar10 <tusshargupta100@gmail.com>
2024-03-11 20:07:32 +0530

    Added download certifiacte functionality

--- src/components/Certificate/Certificate.jsx
+++ src/components/Certificate/Certificate.jsx
@@ -1,10 +1,22 @@
 import moment from 'moment'
 import styles from '../Generation/certificateGenerator.module.scss'
+import { useRef } from 'react'
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 const Certificate = ({ name, course, dateOfConductStart, dateOfConductEnd, signature, signatureDetails }) => {
+  const certificateRef=useRef(null);
+  const handleDownloadCertificate = async () => {
+    html2canvas(certificateRef.current).then(canvas=>{
+      const imgData=canvas.toDataURL('image/png');
+      const pdf=new jsPDF('l',"mm",[1000,670]);
+      pdf.addImage(imgData,'PNG',0,0,1000,667)
+      pdf.save(`${name.split(' ').join('_')}_certificate`)
+    })
+  };
   return (
     <>
-      <div className={styles.certificateWrapper}>
+      <div ref={certificateRef} className={styles.certificateWrapper}>
         <div className={styles.certificateContainer}>
           <div>Logo Here</div>
 
@@ -30,9 +42,8 @@ const Certificate = ({ name, course, dateOfConductStart, dateOfConductEnd, signa
             <span className={styles.smallText}>{signatureDetails}</span>
           </div>
         </div>
-
-        <button style={{ marginTop: ' 3rem' }}>Download PDF</button>
       </div>
+      <button style={{ marginTop: ' 3rem' }} onClick={handleDownloadCertificate}>Download PDF</button>
     </>
   )
 }
