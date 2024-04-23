5d2eb3bf686bb467e0a8b4665887700b8338309e
Tusshar Gupta <131467401+Tusshar10@users.noreply.github.com>
2024-04-23 19:10:18 +0530

    Update Validation.jsx

--- src/components/Validation/Validation.jsx
+++ src/components/Validation/Validation.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Header from '../Header';
 import Header from '../header/Header';
 import "./validation.scss"
 function Validation() {
